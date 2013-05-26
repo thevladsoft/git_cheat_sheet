61aba472412e827163afde59a042ef87a44e8939
vidler <thevladsoft2@gmail.com>
2013-05-25 23:07:42 -0430

    Texto horizontal en los tabs verticales!
    	Listo, ya están. Lo único malo es que uno tiene que
    	definirles el ancho y alto de los tabs en el código.
    	No parece ser autoadaptable.

--- git_cheat_sheet.py
+++ git_cheat_sheet.py
@@ -9,14 +9,34 @@ from PyQt4.QtCore import *
 from PyQt4.QtWebKit import QWebView
 
 #podria usar "apropos git-" para saber las paginas del man que contengan git-etc.
-
+class FingerTabBarWidget(QtGui.QTabBar):
+    def __init__(self, parent=None, *args, **kwargs):
+        self.tabSize = QtCore.QSize(kwargs.pop('width',92), kwargs.pop('height',21))
+        QtGui.QTabBar.__init__(self, parent, *args, **kwargs)
+                 
+    def paintEvent(self, event):
+        painter = QtGui.QStylePainter(self)
+        option = QtGui.QStyleOptionTab()
+ 
+        for index in range(self.count()):
+            self.initStyleOption(option, index)
+            tabRect = self.tabRect(index)
+            tabRect.moveLeft(10)
+            painter.drawControl(QtGui.QStyle.CE_TabBarTabShape, option)
+            painter.drawText(tabRect, QtCore.Qt.AlignVCenter |\
+                             QtCore.Qt.TextDontClip, \
+                             self.tabText(index));
+        painter.end()
+    def tabSizeHint(self,index):
+        return self.tabSize
         
 app = QtGui.QApplication(sys.argv) 
 texty = QtGui.QTextBrowser()
 texty_2 = QtGui.QTextBrowser()
 #vgrid = QtGui.QGridLayout()
 taby = QTabWidget()
-tabu = QTabBar()
+tabu = FingerTabBarWidget()
+#tabu = QTabBar()
 #taby.setTabPosition(QTabWidget.West)
 #tabu.setFixedHeight(60)
 

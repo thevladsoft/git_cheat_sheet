e47b0587f855b41acf8b08a78a477efb00bb369a
vidler <thevladsoft2@gmail.com>
2013-05-26 10:32:30 -0430

    Reacomodo radical, first cleanup
    	Puesto todo en secciones para saber a que pertenece cada cosa.
    	El dif de este con los anteriores es indescifrable
    	Deje los comentarios por ahora por si hacen falta, pero me perdería chequandolos-

--- git_cheat_sheet.py
+++ git_cheat_sheet.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
+#-----Importar delsistema
 import sys
 import subprocess
 #from time import sleep
+#----------/
+#-----Importar de PyQt4
 #from PyQt4 import QtCore, QtGui, QtWebKit
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import QWebView
+#-----Importar adicionales
+#----------/
 
 #podria usar "apropos git-" para saber las paginas del man que contengan git-etc.
+
+#-----Clases
 class FingerTabBarWidget(QtGui.QTabBar):
     def __init__(self, parent=None, *args, **kwargs):
         self.tabSize = QtCore.QSize(kwargs.pop('width',92), kwargs.pop('height',21))
@@ -29,23 +36,49 @@ class FingerTabBarWidget(QtGui.QTabBar):
         painter.end()
     def tabSizeHint(self,index):
         return self.tabSize
-        
+#----------/
+
+#------Declaracion de variable
 app = QtGui.QApplication(sys.argv) 
-texty = QtGui.QTextBrowser()
-texty_2 = QtGui.QTextBrowser()
+#-----|-Widgets
+widget_taby1 = QWidget()
+ventana= QWidget()
 #vgrid = QtGui.QGridLayout()
+#-----|-|-Tabs
 taby = QTabWidget()
 tabu = FingerTabBarWidget()
 #tabu = QTabBar()
 #taby.setTabPosition(QTabWidget.West)
 #tabu.setFixedHeight(60)
+#-----|-|-Botones
+#Estos botones podrian terminar llendose
+boton =  QPushButton("Click me")
+boton2 =  QPushButton("Eat me")
+boton3 =  QPushButton("Drink me")
+#-----|-|-texto
+texty = QtGui.QTextBrowser()
+texty_2 = QtGui.QTextBrowser()
+weby=QWebView()
+#-----|-|-----/
+#-----|-----/
+#-----|-Layouts
+layi_taby1 = QHBoxLayout()
+grid=QGridLayout()
+#-----|-----/
 
 
-
+#-----|-Staticas
 anclas = ["Setup","Config.","Info","Add/Del","Ignoring","Staging","Unstage","Commit"
           ,"Branch.","Merging","Cherry-Pick","Squash.","Conflicts","Sharing","Reverting"
           ,"Fix","Plumbing","Stashing","Remotes","Submodules","Patches","Tags","Archive"
           ,"Instaweb","Env vars.","Hist. change"]
+          
+texto=open("command line ruby cheat sheets.htm").read()
+howto=QUrl(QString("https://www.kernel.org/pub/software/scm/git/docs/howto-index.html"))
+p=app.palette()
+f = taby.font()
+#-----|-----/
+#----------/
 
 #texty = QtGui.QTextEdit()
 #const QUrl uri
@@ -57,61 +90,125 @@ anclas = ["Setup","Config.","Info","Add/Del","Ignoring","Staging","Unstage","Com
 #remoto
 #web.load(QtCore.QUrl("http://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png"))
 #web.show()
-boton =  QPushButton("Click me")
-boton2 =  QPushButton("Eat me")
-boton3 =  QPushButton("Drink me")
-#boton.show()
 
-texto=open("command line ruby cheat sheets.htm").read()
+#boton.show()
+#-----Configuracion inicial de widgets
+
 #texty.setText("<a href=\"http://\">Setup<\a> -----\ngit clone &lt;repo&gt;\n  clone the repository specified by &lt;repo&gt;; this is similar to \"checkout\" in\n  some other version control systems such as Subversion and CVS")
-texty.setText(texto)
-texty.setOpenLinks(False)
+
 #texty.setOpenExternalLinks(True)
 #texty.show()
 
-weby=QWebView()
-
 #vgrid= QVBoxLayout()
 #hgrid= QHBoxLayout()
-grid=QGridLayout()
-grid.setContentsMargins(0,10,0,5)
 
 #hgrid.addWidget(boton)
 #hgrid.addWidget(boton2)
 #hgrid.addWidget(boton3)
-howto=QUrl(QString("https://www.kernel.org/pub/software/scm/git/docs/howto-index.html"))
+ventana.setMinimumSize(150,300)
+ventana.resize(600,630)
+
+#-----|-Textos
 weby.setHtml("<center><p style=\"font-size:300%;color:blue\"><b>Git's How Tos</b></p></center>")
-weby.load(howto)
 
-#def howtos_changing(widget):
-  #taby.setTabText(taby.indexOf(weby),"howtos(Loading...)")
-  ##print("a")
-  
-#def howtos_changed():
-  #taby.setTabText(taby.indexOf(weby),"howtos")
-  ##print("b")
-
-weby.loadStarted.connect(lambda : taby.setTabText(taby.indexOf(weby),"howtos(Loading...)"))
-weby.loadFinished.connect(lambda :taby.setTabText(taby.indexOf(weby),"howtos"))
-
-widget_taby1 = QWidget()
-layi_taby1 = QHBoxLayout()
-widget_taby1.setLayout(layi_taby1)
+#Para quitar el marco alrededor de tabby y por tanto de los widgets que contiene.
 taby.setDocumentMode(True)
 
-#taby.addTab("uno")
-#taby.addTab("dos")
-#taby.addTab(texty,"uno")
-taby.addTab(widget_taby1,"uno")
-taby.addTab(texty_2,"man")
-taby.addTab(weby,"howtos(Loading...)")
-f = taby.font()
 f.setPixelSize(12)
 f.setBold(True)
 taby.tabBar().setFont(f)
 
+texty.setText(texto)
+texty.setOpenLinks(False)
+
+
+
+#-----|-Tabs
+widget_taby1.setStyleSheet(#"QTabBar::tab {font: normal;}"
+                           "QTabBar::tab { border: 3px solid ;"
+                           "border-bottom-color: #C0C0C0;"
+                           "border-right-color: #C0C0C0;"
+                           "border-left-color: #DEDEDE;"
+                           "border-top-color: #DEDEDE;"
+                           #"background: darkgray;"
+                           "background:qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0,"
+                           "stop: 0 "+p.color(QPalette.Mid).name()+","#ffffff,"
+                           "stop: 1.0  "+p.color(QPalette.Light).name()+");" #lightgray);"
+                           "color: black;"
+                           "padding: 5px;"
+                           "border-top-left-radius: 15px;"
+                           "border-bottom-right-radius: 10px ;"
+                           #"border-top-left-radius: 0px;"
+                           #"border-bottom-right-radius: 0px "
+                           "}"
+                           
+                           #"QTabBar::tab:selected { background: gray; } ")
+                           "QTabBar::tab:selected {"  
+                           "background:qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0,"
+                           "stop: 0 "+p.color(QPalette.Dark).name()+","
+                           "stop: 1.0  "+p.color(QPalette.Mid).name()+");}"
+                           )
+                           
+tabu.setUsesScrollButtons(True)
+tabu.setShape(6)  
+
+#-----|-Layouts
+grid.setContentsMargins(0,10,0,5)
+layi_taby1.setContentsMargins(0,0,0,0)     
+layi_taby1.setSpacing(0)
+#-----|-----/
+#----------/
+
+#-----Iniciar Widgets
+#-----|-Primeras cargas
+weby.load(howto)
+#-----|-Crear tabs de los tabbars y tabwidgets
+taby.addTab(widget_taby1,"uno")
+taby.addTab(texty_2,"man")
+taby.addTab(weby,"howtos(Loading...)")
 for x in anclas:
     tabu.addTab(x)
+#-----|-Llenar layouts
+#-----|-|-grid es el Layout principal
+grid.addWidget(boton,0,0)
+grid.addWidget(boton2,0,1)
+grid.addWidget(boton3,0,2)
+grid.addWidget(taby,1,0,1,3)
+#-----|-|-layi_taby es el Layout asignado a cada tab(1 es el primero)
+layi_taby1.addWidget(tabu)
+layi_taby1.addWidget(texty)
+#-----|-|-----/
+#-----|-----/
+#----------/
+
+#-----Asignar layouts
+ventana.setLayout(grid)
+#-----|-Asignar los de los widgets en los tabs (widget_taby'n' es el widget asignado a al tab 'n')
+widget_taby1.setLayout(layi_taby1)
+#-----|-----/
+#----------/
+#def howtos_changing(widget):
+  #taby.setTabText(taby.indexOf(weby),"howtos(Loading...)")
+  ##print("a")
+#def howtos_changed():
+  #taby.setTabText(taby.indexOf(weby),"howtos")
+  ##print("b")
+
+
+
+
+
+
+
+#taby.addTab("uno")
+#taby.addTab("dos")
+#taby.addTab(texty,"uno")
+
+
+
+
+
+
 #tabu.addTab("a")
 #tabu.addTab("b")
 #tabu.tabRect(1).setHeight(100)
@@ -121,7 +218,7 @@ for x in anclas:
 #widget_taby1.setStyleSheet("QTabBar {border-top-left-radius: 4px}")
 #widget_taby1.setStyleSheet("QTabBar::tab { border: 3px solid ;border-top-color: #C0C0C0;border-left-color: #C0C0C0;border-right-color: #DEDEDE;border-bottom-color: #DEDEDE;background: darkgray; color: white; padding: 5px;border-top-left-radius: 15px;border-bottom-right-radius: 10px }QTabBar::tab:selected { background: gray; } ")
 #taby.setObjectName("taby")
-p=app.palette()
+
 print p.color(QPalette.Window).name()
 #app.setStyleSheet("QTabBar::tab { border: 3px solid ;border-bottom-color: "+p.color(QPalette.Window).name() +";"##C2C7CB;"
                            #"border-right-color: "+p.color(QPalette.Window).name() +";"#C0C0C0;"
@@ -167,32 +264,7 @@ print p.color(QPalette.Window).name()
                            #"font: bold;"
                            #)
 #taby.setStyleSheet("padding: 6px")
-layi_taby1.setContentsMargins(0,0,0,0)     
-layi_taby1.setSpacing(0)
-widget_taby1.setStyleSheet(#"QTabBar::tab {font: normal;}"
-                           "QTabBar::tab { border: 3px solid ;"
-                           "border-bottom-color: #C0C0C0;"
-                           "border-right-color: #C0C0C0;"
-                           "border-left-color: #DEDEDE;"
-                           "border-top-color: #DEDEDE;"
-                           #"background: darkgray;"
-                           "background:qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0,"
-                           "stop: 0 "+p.color(QPalette.Mid).name()+","#ffffff,"
-                           "stop: 1.0  "+p.color(QPalette.Light).name()+");" #lightgray);"
-                           "color: black;"
-                           "padding: 5px;"
-                           "border-top-left-radius: 15px;"
-                           "border-bottom-right-radius: 10px ;"
-                           #"border-top-left-radius: 0px;"
-                           #"border-bottom-right-radius: 0px "
-                           "}"
-                           
-                           #"QTabBar::tab:selected { background: gray; } ")
-                           "QTabBar::tab:selected {"  
-                           "background:qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0,"
-                           "stop: 0 "+p.color(QPalette.Dark).name()+","
-                           "stop: 1.0  "+p.color(QPalette.Mid).name()+");}"
-                           )
+
                            
 #taby.setBackgroundRole(QPalette.Base)
 #taby.setAutoFillBackground(True)
@@ -204,33 +276,27 @@ widget_taby1.setStyleSheet(#"QTabBar::tab {font: normal;}"
 
 #tabu.setTabPosition(QTabWidget.West)
 print tabu.backgroundRole()
-tabu.setUsesScrollButtons(True)
 
-tabu.setShape(6)
 
 #grid.addWidget(taby,0,0,1,3)
 
-grid.addWidget(boton,0,0)
-grid.addWidget(boton2,0,1)
-grid.addWidget(boton3,0,2)
+
 
 #vgrid.addWidget(hgrid.widget())
 #vgrid.addWidget(texty)
 
 #grid.addWidget(texty,2,0,1,3)
-grid.addWidget(taby,1,0,1,3)
+
 
 #grid.addWidget(tabu,1,0,1,1)
-layi_taby1.addWidget(tabu)
-layi_taby1.addWidget(texty)
 
-ventana= QWidget()
+
+
 #ventana.setLayout(vgrid)
-ventana.setLayout(grid)
-ventana.setMinimumSize(150,300)
-ventana.resize(600,630)
+#ventana.setLayout(grid)
+
 #ventana.setMaximumSize(800,800)
-ventana.show()
+
 
 #setLayout(vgrid)
 
@@ -249,6 +315,9 @@ ventana.show()
 #def anchory(i):
    #texty.scrollToAnchor(anclas[i])
 
+#-----Connects
+weby.loadStarted.connect(lambda : taby.setTabText(taby.indexOf(weby),"howtos(Loading...)"))
+weby.loadFinished.connect(lambda :taby.setTabText(taby.indexOf(weby),"howtos"))
 #QObject.connect(texty,SIGNAL("anchorClicked(QUrl)"),printuri)
 QObject.connect(texty,SIGNAL("anchorClicked(QUrl)"),lambda url:(taby.setCurrentIndex(taby.indexOf(texty_2)),texty_2.setHtml(subprocess.Popen(["man", "--troff-device=html" , url.toString()],stdout=subprocess.PIPE).communicate()[0])))
 #QObject.connect(boton,SIGNAL("anchorClicked(QUrl)"),printuri)
@@ -258,5 +327,16 @@ boton2.clicked.connect(lambda:weby.load(howto))
 tabu.currentChanged.connect(lambda i: texty.scrollToAnchor(anclas[i]))
 #def anchorClicked(uri):
 #  print uri
-
+#----------/
+#-----Ultimos pasos
+ventana.show()
+#----------/
+#-----Comenzar la corrida
 sys.exit(app.exec_()) 
+#----------/
+#-----/
+#----/
+#---/
+#--/
+#-/
+#/
